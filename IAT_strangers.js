730ad82ad309d23e471e8a74966afde8c1511490
sofibonch <84900142+sofibonch@users.noreply.github.com>
2022-04-05 09:31:08 +0300

    Create IAT_strangers.js

---
+++ IAT_strangers.js
@@ -0,0 +1,61 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+	
+	var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Americans', //Will appear in the data.
+		    title : {
+		      media : {word : 'Americans'}, //Name of the category presented in the task.
+		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    	{image : 'str1.png'}, 
+    			{image : 'str2.png'}, 
+    			{image : 'str3.png'}, 
+    			{image : 'str4.png'}
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#31b404','font-size':'3em'}
+		  },	
+
+  		attribute1 : 
+			{
+			name : 'Republican', //Attribute label
+			title : {
+				media : {word : 'Republican'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{image : 'rep1.png'},
+				{image : 'rep2.png'},
+				{image : 'rep3.png'},
+				{image : 'rep4.png'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+		attribute2 : 
+			{
+			name : 'Democrats', //Attribute label
+			title : {
+				media : {word : 'Democarats'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{image : 'dem1.png'},
+				{image : 'dem2.png'},
+				{image : 'dem3.png'},
+				{image : 'dem4.png'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+
+  base_url : {//Where are your images at?
+    image : 'https://sofibonch.github.io/ST-IAT/images/'
+  }}
+  );
+  });
